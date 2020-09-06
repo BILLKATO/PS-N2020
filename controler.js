a14c1742c60b88955edc3aff64f3224600407685
BILLKATO <biel.mk@hotmail.com>
2020-09-06 19:51:13 -0300

    Revert "PS - N2020 V0.3"
    This reverts commit b89f017347438bf45f421f9c0c0b5fa0bf15495d.

--- controler.js
+++ controler.js
@@ -209,15 +209,10 @@ function execSQLQuery(sqlQry, res, up){
 
 
   connection.query(sqlQry, function(error, results, fields){
-
 	 if(error == null)
 	 {
-	  if(results.affectedRows != 0 && results.affectedRows != null && results.affectedRows != undefined && sqlQry.indexOf("INSERT") != -1 )
+	  if(results.affectedRows != 0 && results.affectedRows != null && results.affectedRows != undefined)
 	   res.json("Registro Inserido!");
-	  else if(results.affectedRows != 0 && results.affectedRows != null && results.affectedRows != undefined && sqlQry.indexOf("UPDATE") != -1)
-	   res.json("Registro Atualizado!");
-	  else if(results.affectedRows != 0 && results.affectedRows != null && results.affectedRows != undefined && sqlQry.indexOf("DELETE") != -1)
-	   res.json("Registro Deletado!");
 	  else
  	   res.json(results);
  	 }
